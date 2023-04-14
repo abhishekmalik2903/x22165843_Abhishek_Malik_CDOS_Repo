e3cd048a157f35d2a08b641ff016e9aa0161cc59
EC2 Default User <ec2-user@ip-172-31-12-174.eu-west-1.compute.internal>
2023-04-14 01:45:05 +0000

    test

--- mypro/settings.py
+++ mypro/settings.py
@@ -120,7 +120,7 @@ STATICFILES_DIRS = [
 
 STATIC_URL = '/static/'
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
