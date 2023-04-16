b096e013c9d507934a051bc7f453ba428275e77b
EC2 Default User <ec2-user@ip-172-31-12-174.eu-west-1.compute.internal>
2023-04-16 00:07:00 +0000

    commit 5

--- mypro/settings.py
+++ mypro/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m57seh$t$+^=uaa4s-ftyo_^gg8lg^_$48qo$o*d@@*xa^@n1j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8927ec779ca64b4db5172bf0a23174c6.vfs.cloud9.eu-west-1.amazonaws.com','x22165843abhishekmalikcdosapplicatio-env.eba-fcv2p6cj.eu-west-3.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['8927ec779ca64b4db5172bf0a23174c6.vfs.cloud9.eu-west-1.amazonaws.com','x22165843abhishekmalikcdos-env.eba-3zyg3db2.eu-west-1.elasticbeanstalk.com']
 
 
 # Application definition
